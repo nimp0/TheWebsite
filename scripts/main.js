b138c40eaf9972e23184aef3d2fb01e9c3f8f8cc
Nimpo <cloud_nimpo@ukr.net>
2019-05-28 21:54:19 +0300

    #firstRun

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+//Funny actors
+var simon = document.getElementById("simon");
+var bruce = document.getElementById("bruce");
+var ben = document.getElementById("ben");
+
+simon.addEventListener("click", picLink);
+bruce.addEventListener("click", picLink);
+ben.addEventListener("click", picLink);
+
+function picLink() {
+  var allImages = document.querySelectorAll(".partnerSection img");
+
+  for (var i = 0; i < allImages.length; i++) {
+    allImages[i].className = "hide";
+  }
+
+  var picId = this.attributes["data-img"].value;
+  var pic = document.getElementById(picId);
+  if (pic.className)
+  {
+    pic.className=(pic.className=='hide')?'':'hide';
+  } 
+}
+
+
+
+
+
+
+ 
+
+
